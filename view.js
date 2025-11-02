37646468abb3bbc10604ea2d7c42f90bdd90a441
An Zhang <an.zhang@wustl.edu>
2025-11-02 16:54:39 -0600

    get rid of "" from Edit

--- view.js
+++ view.js
@@ -33,7 +33,7 @@ function make_file_element(file_obj) {
         return $(`<div class='file'  id="${file_hash}_div">
             <span class="oi oi-file" id="${file_hash}_icon"/> ${file_obj.filename}
             <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> "Edit"
+                <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> Edit
             </button>
         </div>`)
     }
